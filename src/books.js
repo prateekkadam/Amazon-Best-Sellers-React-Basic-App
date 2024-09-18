377e0dd7248fc885b30507b662d1590646998826
Prateekkadam <pratikkadam17670@gmail.com>
2024-09-18 15:57:32 +0530

    First Commit

---
+++ src/books.js
@@ -0,0 +1,25 @@
+import img1 from './images/book-1.jpg'
+import img2 from './images/book-2.jpg'
+import img3 from './images/book-3.jpg'
+
+export const books = [
+  {
+    author: 'Calley Means',
+    title: 'Good Energy',
+    img: img1,
+    id: 1,
+  },
+  {
+    author: 'Katherine Rundell',
+    title: 'Impossible Creatures',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'Peter Brown',
+    title: 'The Wild Robot',
+    img: img3,
+    id: 3,
+  },
+]
+
